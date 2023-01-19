73aa7d9ed967c99a0e799fb50357dec4873063da
YiranZhang-37 <80355510+YiranZhang-37@users.noreply.github.com>
2023-01-19 00:24:40 -0500

    Update Yiran_Zhang_STATS790_Question2.R
    Clear the format.

--- Assignment 1/Yiran_Zhang_STATS790_Question2.R
+++ Assignment 1/Yiran_Zhang_STATS790_Question2.R
@@ -52,17 +52,23 @@ linear <- function(x) b0+b1*x
 x <- seq(from = -1.5, to = 4, by = 0.05)
 y <- seq(from = -2, to = 3.5, by = 0.05)
 background <- expand.grid(x,y) # entire background
-y_hat <- linear(x) # filter for the linear regression line
+y_hat <- linear(x) 
+# y_hat is the filter for the linear regression line 
+# that separate blue and orange dots on the background
 
-blue_pt <- background %>% filter(Var2 < y_hat) 
-orange_pt <- background %>% filter(Var2 > y_hat)
+blue_pt <- (background %>% filter(Var2 < y_hat)) # lower part is blue dots
+orange_pt <- (background %>% filter(Var2 > y_hat)) # upper part is orange dots
 
-# Figure
-plot(blue_obs, col='deepskyblue', xlim = c(-1.5,4), ylim = c(-2,3.5), 
-     xlab='', ylab='', lwd=3, cex = 1.5, xaxt='n', yaxt='n')
-points(orange_obs, col='orange', lwd = 3, cex = 1.5)
-abline(linear_model, col = 'black', lwd = 3)
-points(orange_pt, col='orange', pch=20, cex = 0.3)
-points(blue_pt, col='deepskyblue', pch=20, cex = 0.3)
+# Figure reproduction
+plot(blue_obs
+     , col='deepskyblue' # set point color to blue
+     , xlim = c(-1.5,4) # expand x axis to fit orange observations later
+     , ylim = c(-2,3.5) # expand y axis to fit orange observations later
+     , xlab='', ylab='' # clear x and y labels
+     , lwd=3, cex = 1.5 # increase size and thickness of the observations
+     , xaxt='n', yaxt='n') # clear the x and y axis number labels
+points(orange_obs, col='orange', lwd = 3, cex = 1.5) # add orange observations
+abline(linear_model, col = 'black', lwd = 3) # add the decision boundary
+points(orange_pt, col='orange', pch=20, cex = 0.3) # add orange background dots
+points(blue_pt, col='deepskyblue', pch=20, cex = 0.3) # add blue background dots
 
-# This assignment is awesome!
